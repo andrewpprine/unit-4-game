39581190a75d44bf5a6937a2fe07f0553c1d53e3
Andrew Prine <andrewpprine@gmail.com>
2018-06-19 13:18:08 -0700

    vars and pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+var ranNumTarget = Math.ceil(Math.random()*(120-19)+19);
+var ranNumCrystal = Math.ceil(Math.random()*(12-1)+1);
+var totalWins
+var totalLosses
+
+//upon new game, assign values to crystals
+//upon new game, assign target score
+
+//after clicking crystal, add value to current score
+
+//if current score < target score, continue
+//if current score = target score, player wins
+//if current score > target score, player loses
+
+//if player wins, add 1 to totalWins
+//if player loses, add 1 to totalLosses
